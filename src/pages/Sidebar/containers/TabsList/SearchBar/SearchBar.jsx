52484a4b5ab605fe075db9319425652a7672980a
lxieyang <lxieyang@umich.edu>
2019-08-08 11:23:35 -0400

    optimize search result indicator

--- src/pages/Sidebar/containers/TabsList/SearchBar/SearchBar.jsx
+++ src/pages/Sidebar/containers/TabsList/SearchBar/SearchBar.jsx
@@ -9,12 +9,36 @@ const SearchBar = ({
   return (
     <div className="SearchBarContainer">
       <input
+        type="search"
         className="SearchBarInput"
         placeholder={'🔍 search tabs here'}
         value={searchBarInputText}
         onChange={(e) => handleSearchBarInputText(e)}
       />
-      <div className="SearchResultsCount">{searchCount}</div>
+      <div className="SearchResultsCountContainer">
+        <div
+          className={[
+            'SearchResultsCount',
+            searchBarInputText.length > 0 && searchCount === 0
+              ? 'NoResults'
+              : null,
+            searchBarInputText.length > 0 && searchCount === 1
+              ? 'Success'
+              : null,
+            searchBarInputText.length > 0 && searchCount > 1
+              ? 'Searching'
+              : null,
+            // searchBarInputText.length > 0 && searchCount > 1 && searchCount <= 3
+            //   ? 'CloseToSuccess'
+            //   : null,
+            // searchBarInputText.length > 0 && searchCount > 3
+            //   ? 'Searching'
+            //   : null,
+          ].join(' ')}
+        >
+          {searchCount}
+        </div>
+      </div>
     </div>
   );
 };
